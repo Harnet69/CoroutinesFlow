8c63dbbad3ea0eb6d203b83dc1887c30b0d1e655
Harnet69 <harnet69@gmail.com>
2020-12-29 12:39:42 +0100

    Articles list is shown

--- app/src/main/java/com/harnet/coroutinesflow/view/ArticlesFragment.kt
+++ app/src/main/java/com/harnet/coroutinesflow/view/ArticlesFragment.kt
@@ -1,20 +1,22 @@
 package com.harnet.coroutinesflow.view
 
 import android.graphics.Color
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.harnet.coroutinesflow.viewModel.ArticlesViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.harnet.coroutinesflow.R
+import com.harnet.coroutinesflow.adapter.ArticlesAdapter
+import com.harnet.coroutinesflow.viewModel.ArticlesViewModel
 import kotlinx.android.synthetic.main.articles_fragment.*
 
 class ArticlesFragment : Fragment() {
-
+    private lateinit var articlesAdapter: ArticlesAdapter
     private lateinit var viewModel: ArticlesViewModel
 
     override fun onCreateView(
@@ -28,22 +30,47 @@ class ArticlesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observeModel()
+        articlesAdapter = ArticlesAdapter(arrayListOf())
+
         viewModel.refresh()
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = articlesAdapter
+        }
+
+        // add separation line between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        observeModel()
     }
 
     private fun observeModel(){
         viewModel.mArticlesList.observe(viewLifecycleOwner, Observer { articles ->
-            Log.i("ArticlesShowing", "observeModel: $articles")
-            recyclerView.setBackgroundColor(Color.CYAN)
+            articles?.let {
+                if(it.isNotEmpty()){
+                    articlesAdapter.updateArticlesList(articles)
+                    recyclerView.setBackgroundColor(Color.CYAN)
+                    recyclerView.visibility = View.VISIBLE
+                }
+
+            }
         })
 
         viewModel.mIsLoading.observe(viewLifecycleOwner, Observer {
-            if(it)
+            if(it) {
+                recyclerView.visibility = View.INVISIBLE
                 progressBar.visibility = View.VISIBLE
-            else
+            }
+            else {
+                recyclerView.visibility = View.VISIBLE
                 progressBar.visibility = View.INVISIBLE
-
+            }
         })
     }
 }
